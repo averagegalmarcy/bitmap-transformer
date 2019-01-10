d1b93db5deba3e2130bfdd714bee5821be54a265
Marcy Silverman <marcysilve225@gmail.com>
2019-01-10 09:32:51 -0800

    did some timeouts

---
+++ callback.js
@@ -0,0 +1,11 @@
+setTimeout(() => console.log('hi'), 1000);
+
+setTimeout(() => console.log('hello'), 2000);
+
+setInterval(() => console.log('whatup'), 1000);
+// using a function
+// const test = () => {
+//   console.log('what up');
+// };
+// setInterval(test, 1000);
+
